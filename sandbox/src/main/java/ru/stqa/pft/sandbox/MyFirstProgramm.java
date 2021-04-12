2b34631c92cd9f1eec593a477e04db20d09aaf95
dbashkirov88 <75624396+dbashkirov88@users.noreply.github.com>
2021-04-12 16:49:25 +0300

    переменные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -4,6 +4,11 @@ public class MyFirstProgramm {
 	
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
+
+		int l = 8;
+		int s = l * l;
+		System.out.println("Плащадь квадрата со стороной " + l + " = " + s);
+
 	}
 	
 }
